1bb4df58c9359a2a723938ae21a2974809ba79b0
gotjosh <github@begreen.nu>
2018-06-27 21:43:21 +0100

    antd sider menu integration

--- imports/ui/layouts/App.jsx
+++ imports/ui/layouts/App.jsx
@@ -14,8 +14,14 @@ import ListPageContainer from '../containers/ListPageContainer.jsx';
 import AuthPageSignIn from '../pages/AuthPageSignIn.jsx';
 import AuthPageJoin from '../pages/AuthPageJoin.jsx';
 import NotFoundPage from '../pages/NotFoundPage.jsx';
+import ListHeader from '../components/ListHeader.jsx';
 
 const CONNECTION_ISSUE_TIMEOUT = 5000;
+
+import { Layout, Menu, Breadcrumb, Icon } from 'antd';
+
+const { Header, Content, Footer, Sider } = Layout;
+const SubMenu = Menu.SubMenu;
 
 export default class App extends Component {
   static getDerivedStateFromProps(nextProps) {
@@ -36,8 +42,8 @@ export default class App extends Component {
       defaultList: null,
       redirectTo: null,
     };
-    this.toggleMenu = this.toggleMenu.bind(this);
-    this.closeMenu = this.toggleMenu.bind(this, false);
+    // this.toggleMenu = this.toggleMenu.bind(this);
+    // this.closeMenu = this.toggleMenu.bind(this, false);
     this.logout = this.logout.bind(this);
   }
 
@@ -48,9 +54,14 @@ export default class App extends Component {
     }, CONNECTION_ISSUE_TIMEOUT);
   }
 
-  toggleMenu() {
-    this.props.menuOpen.set(!this.props.menuOpen.get());
-  }
+  // toggleMenu() {
+  //   this.props.menuOpen.set(!this.props.menuOpen.get());
+  // }
+  toggle = () => {
+    this.setState({
+      collapsed: !this.state.collapsed,
+    });
+  };
 
   logout() {
     Meteor.logout();
@@ -70,67 +81,123 @@ export default class App extends Component {
     }
     return redirect;
   }
+  state = {
+    collapsed: false,
+  };
+
+  onCollapse = (collapsed) => {
+    console.log(collapsed);
+    this.setState({ collapsed });
+  };
 
   renderContent(location) {
-    const {
-      user,
-      connected,
-      lists,
-      menuOpen,
-      loading,
-    } = this.props;
+    const { user, connected, lists, menuOpen, loading } = this.props;
     const { showConnectionIssue } = this.state;
 
     const commonChildProps = {
       menuOpen: this.props.menuOpen,
     };
-
     return (
-      <div id="container" className={menuOpen ? 'menu-open' : ''}>
-        <section id="menu">
-          <LanguageToggle />
-          <UserMenu user={user} logout={this.logout} />
-          <ListList lists={lists} />
-        </section>
-        {showConnectionIssue && !connected
-          ? <ConnectionNotification />
-          : null}
-        <div className="content-overlay" onClick={this.closeMenu} />
-        <div id="content-container">
-          {loading ? (
-            <Loading key="loading" />
-          ) : (
-            <TransitionGroup>
-              <CSSTransition
-                key={location.key}
-                classNames="fade"
-                timeout={200}
-              >
-                <Switch location={location}>
-                  <Route
-                    path="/lists/:id"
-                    render={({ match }) => (
-                      <ListPageContainer match={match} {...commonChildProps} />
-                    )}
-                  />
-                  <Route
-                    path="/signin"
-                    render={() => <AuthPageSignIn {...commonChildProps} />}
-                  />
-                  <Route
-                    path="/join"
-                    render={() => <AuthPageJoin {...commonChildProps} />}
-                  />
-                  <Route
-                    path="/*"
-                    render={() => <NotFoundPage {...commonChildProps} />}
-                  />
-                </Switch>
-              </CSSTransition>
-            </TransitionGroup>
-          )}
-        </div>
-      </div>
+      <Layout style={{ minHeight: '100vh' }}>
+        <Sider
+          width="300px"
+          trigger={null}
+          collapsible
+          collapsed={this.state.collapsed}
+          onCollapse={this.onCollapse}
+        >
+          <Menu
+            theme="dark"
+            defaultSelectedKeys={['1']}
+            mode="inline"
+            style={{ paddingRight: '30px' }}
+          >
+            <Menu.Item key="0">
+              <Icon
+                style={{ fontSize: '150%' }}
+                className="trigger"
+                type={this.state.collapsed ? 'menu-unfold' : 'menu-fold'}
+                onClick={this.toggle}
+              />
+              <span>Slide Closed</span>
+            </Menu.Item>
+            <Menu.Item key="1">
+              <Icon type="global" />
+              <span>
+                <LanguageToggle style={{ paddingLeft: '36px' }} />
+              </span>
+            </Menu.Item>
+            <SubMenu
+              key="sub0"
+              title={
+                <span>
+                  <Icon type="user" />
+                  <span>User</span>
+                </span>
+              }
+            >
+              <UserMenu user={user} logout={this.logout} />
+            </SubMenu>
+            <SubMenu
+              key="sub1"
+              title={
+                <span>
+                  <Icon type="check-square-o" />
+                  <span>Lists</span>
+                </span>
+              }
+            >
+              <ListList lists={lists} />
+            </SubMenu>
+          </Menu>
+        </Sider>
+        <Layout>
+          <Content style={{ margin: '0 16px' }}>
+            {showConnectionIssue && !connected ? (
+              <ConnectionNotification />
+            ) : null}
+            <div className="content-overlay" onClick={this.closeMenu} />
+            {loading ? (
+              <Loading key="loading" />
+            ) : (
+              <TransitionGroup>
+                <CSSTransition
+                  key={location.key}
+                  classNames="fade"
+                  timeout={200}
+                >
+                  <Switch location={location}>
+                    <Route
+                      path="/lists/:id"
+                      render={({ match }) => (
+                        <ListPageContainer
+                          match={match}
+                          {...commonChildProps}
+                        />
+                      )}
+                    />
+                    <Route
+                      path="/signin"
+                      render={() => <AuthPageSignIn {...commonChildProps} />}
+                    />
+                    <Route
+                      path="/join"
+                      render={() => <AuthPageJoin {...commonChildProps} />}
+                    />
+                    <Route
+                      path="/*"
+                      render={() => <NotFoundPage {...commonChildProps} />}
+                    />
+                  </Switch>
+                </CSSTransition>
+              </TransitionGroup>
+            )}
+          </Content>
+          <Footer style={{ textAlign: 'center' }}>
+            Ant Design ©2016 Created by Ant UED
+          </Footer>
+        </Layout>
+      </Layout>
     );
   }
 
@@ -138,9 +205,9 @@ export default class App extends Component {
     return (
       <BrowserRouter>
         <Route
-          render={({ location }) => (
+          render={({ location }) =>
             this.renderRedirect(location) || this.renderContent(location)
-          )}
+          }
         />
       </BrowserRouter>
     );
